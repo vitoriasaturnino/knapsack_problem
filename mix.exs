a5a0da4510450025d24ee5e273f5da28596660f8
vitoriasaturnino <vim.saturnino@gmail.com>
2023-11-13 22:06:50 -0300

    chore: Initialize Mix project for Knapsack problem

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule KnapsackProblem.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16-rc",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: [main_module: Knapsack.Main]
     ]
   end
 
