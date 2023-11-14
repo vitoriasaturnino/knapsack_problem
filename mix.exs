854fc6a05211dd8cdaec634a754f5e2a4b1baade
vitoriasaturnino <vim.saturnino@gmail.com>
2023-11-14 17:33:15 -0300

    build: update elixir to a stable version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule KnapsackProblem.MixProject do
     [
       app: :knapsack_problem,
       version: "0.1.0",
-      elixir: "~> 1.16-rc",
+      elixir: "~> 1.15.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       escript: [main_module: Knapsack.Main]
